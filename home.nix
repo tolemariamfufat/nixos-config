0157b6e938a92ea624b0fb3a08cb0a77488e7221
to <to@nixos.local>
2026-07-24 06:10:31 -0400

    Add waybar status bar

--- home.nix
+++ home.nix
@@ -1,33 +1,61 @@
 { config, pkgs, ... }:
 
 {
-  # Replace 'to' with your username
   home.username = "to";
   home.homeDirectory = "/home/to";
-
-  # Set stateVersion to the NixOS version you started with
   home.stateVersion = "24.05"; 
 
-  # User-specific packages
-  home.packages = with pkgs; [
-    # Packages for your user account only (e.g., neofetch, htop, etc.)
-  ];
- 
- # Enable and configure Kitty terminal
+  # Enable Kitty
   programs.kitty = {
     enable = true;
     settings = {
       confirm_os_window_close = 0;
-      background_opacity = "0.9"; # Optional: sleek slight transparency
+      background_opacity = "0.9";
     };
   };
- # Enable Rofi (Wayland fork)
+
+  # Enable Rofi
   programs.rofi = {
     enable = true;
     package = pkgs.rofi;
   };
 
-  # Manage programs declaratively
+  # Enable and configure Waybar
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        modules-left = [ "hyprland/workspaces" "hyprland/window" ];
+        modules-center = [ "clock" ];
+        modules-right = [ "pulseaudio" "network" "cpu" "memory" "tray" ];
+
+        clock = {
+          format = "{:%Y-%m-%d %H:%M}";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
+        cpu = {
+          format = "CPU {usage}%";
+        };
+        memory = {
+          format = "RAM {}%";
+        };
+        network = {
+          format-wifi = "WiFi {signalStrength}%";
+          format-ethernet = "Eth {ipaddr}";
+          format-disconnected = "Disconnected";
+        };
+        pulseaudio = {
+          format = "Vol {volume}%";
+          format-muted = "Muted";
+        };
+      };
+    };
+  };
+
+  # Git settings
   programs.git = {
     enable = true;
     settings = {
@@ -38,6 +66,5 @@
     };
   };
 
-  # Let Home Manager manage itself
   programs.home-manager.enable = true;
 }
