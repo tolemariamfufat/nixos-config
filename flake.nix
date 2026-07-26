c7affa9feba451e10448bdc7048c35b825c744b0
Tolemariam Fufa Teso <99896370+tolemariamfufat@users.noreply.github.com>
2026-07-26 03:36:22 -0400

    Update Nixpkgs and Home Manager versions

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS system configuration with Home Manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # or your release branch
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -16,13 +16,11 @@
       modules = [
         ./configuration.nix
 
-        # Add Home Manager as a module
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          # Replace 'to' with your exact username
           home-manager.users.to = import ./home.nix;
         }
       ];
